e77d48afda6210242049f6ec3c45e1080353b8d3
Werner Reyes <reyespilcowerner@gmail.com>
2025-04-17 18:54:58 -0500

    fix: Ajustar el ancho del diálogo offline y corregir el mensaje de error de conexión

--- src/presentation/pages/private/components/OfflineDialog.tsx
+++ src/presentation/pages/private/components/OfflineDialog.tsx
@@ -4,6 +4,7 @@ import { useDispatch } from "react-redux";
 
 export const OfflineDialog = () => {
   const dispatch = useDispatch();
+
   return (
     <Offline
       onChange={(online) => {
@@ -12,17 +13,18 @@ export const OfflineDialog = () => {
     >
       <Dialog
         style={{
-          maxWidth: "30vw",
+          width: "30rem",
         }}
+        baseZIndex={9999}
         closable={false}
-        className="bg-white rounded-2xl shadow-lg p-8 mx-4 text-center"
+        className="bg-white rounded-2xl shadow-lg p-8  text-center"
         visible
         onHide={() => {}}
       >
         <div className="w-24 h-24 bg-primary/10 rounded-full flex items-center justify-center mx-auto mb-4">
           <i className="pi pi-wifi text-4xl text-primary" />
         </div>
-        <h3 className="text-xl font-bold mb-2">Whoops! No hay señal</h3>
+        <h3 className="text-xl font-bold mb-2">Whoops! No hay internet</h3>
         <p className="text-gray-500 mb-6">
           Parece que estás en una zona sin conexión a internet. ¡No te
           preocupes, estamos trabajando para reconectar!
