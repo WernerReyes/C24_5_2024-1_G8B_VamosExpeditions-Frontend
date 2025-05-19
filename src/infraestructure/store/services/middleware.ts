7e6b07850512e27c03d6dbe3993270968455ae6e
Werner Reyes <reyespilcowerner@gmail.com>
2025-05-18 19:09:02 -0500

    refactor(middleware): update error logging to use action.payload

--- src/infraestructure/store/services/middleware.ts
+++ src/infraestructure/store/services/middleware.ts
@@ -4,13 +4,14 @@ import type { MiddlewareAPI, Middleware } from "@reduxjs/toolkit";
 
 const { ENV_MODE } = constantEnvs;
 /**
- * Log a warning and show a toast!
+ * Log a warning and show a message in the extension if a network or cache error occurs.
+ * Useful for API calls that are expected to fail, etc.
  */
 export const rtkQueryErrorLogger: Middleware =
   (_: MiddlewareAPI) => (next) => (action) => {
     if (isRejectedWithValue(action)) {
       if (ENV_MODE === "development") {
-        console.error("Error: ", action.error);
+        console.error("Error: ", action.payload);
       }
     }
 
